dfe9dca6afd5d335ce310b965c30512d44916e87
Sohail Khan Shinwari <sohail8338@gmail.com>
2024-01-06 08:34:53 +0500

    Livewire customer Pagination
    The Laravel Pagination create an issue when we enter some record and then click on pagination button it create an issue as the link was changed in that. So i have set for the Livewire component.

--- pagination/customePagination.blade.php
+++ pagination/customePagination.blade.php
@@ -12,12 +12,12 @@
                 </svg>
             </button>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-300">
+            <button wire:click="previousPage" class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-300">
                 <span class="sr-only">Previous</span>
                 <svg class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M14.707 15.707a1 1 0 01-1.414 0l-5.657-5.657a1 1 0 010-1.414l5.657-5.657a1 1 0 111.414 1.414L9.172 9.293l5.535 5.535a1 1 0 010 1.414z" clip-rule="evenodd" />
                 </svg>
-            </a>
+            </button>
         @endif
         @foreach ($elements as $element)
             @if (is_array($element))
@@ -25,18 +25,20 @@
                     @if ($page == $paginator->currentPage())
                         <button class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center" disabled>{{ $page }}</button>
                     @else
-                        <a href="{{ $url }}" class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-300">{{ $page }}</a>
-                    @endif
+                    <!-- wire:click="gotoPage({{ $page }})" -->
+                        <button wire:click="gotoPage({{ $page }})" class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-300"> {{ $page }}</button>
+                        <!-- <span class="bg-green text-gray-500"> {{$url}} </span> -->
+                        @endif
                 @endforeach
             @endif
         @endforeach
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-300">
+            <button wire:click="nextPage" class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center hover:bg-gray-300">
                 <span class="sr-only">Next</span>
                 <svg class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M5.293 4.293a1 1 0 011.414 0l5.657 5.657a1 1 0 010 1.414l-5.657 5.657a1 1 0 11-1.414-1.414L10.828 10l-5.535-5.535a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg>
-            </a>
+            </button>
         @else
             <button class="bg-gray-200 text-gray-900 font-semibold rounded-full w-8 h-8 flex items-center justify-center" disabled>
                 <span class="sr-only">Next</span>
